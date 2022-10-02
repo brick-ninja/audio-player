ac3c823cf0222bea532e453187ab2a0ff9997856
brick-ninja <davidliang0424@gmail.com>
2022-10-03 00:07:56 +0800

    working layout

---
+++ components/sidebar.tsx
@@ -0,0 +1,16 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from '@chakra-ui/layout'
+
+const Sidebar = () => {
+  return <div>Hello from sidebar</div>
+}
+
+export default Sidebar

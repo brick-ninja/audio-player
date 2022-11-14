81877935e390727c08e85fb095d700815121d0b2
brick-ninja <davidliang0424@gmail.com>
2022-11-14 17:25:37 +0800

    add gradient component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import GradientLayout from '../components/gradientLayout'
 import styles from '../styles/Home.module.css'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <GradientLayout color="green" roundImage>
+      <div>home page</div>
+    </GradientLayout>
+  )
 }
 
 export default Home
